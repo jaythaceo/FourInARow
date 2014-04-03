f23306ec2b5757ddcd030f1dd052fff267555766
jaythaceo <jaythaceo@gmail.com>
2014-04-03 16:08:22 -0400

    catching up

--- src/connect.py
+++ src/connect.py
@@ -1,4 +1,3 @@
-# Graphical Four-In-A-Row ( Connect Four clone )
 # By: Jason Brooks
 # March 31, 2014
 # Email: jaythaceo@gmail.com
@@ -45,7 +44,7 @@ def main():
     blackTokenImg = pygame.image.load('4row_black.png')
     blackTokenImg = pygame.transform.smoothscale(redTokenImg, (SPAECSIZE, SPAECSIZE))
     boardImg = pygame.image.load('4row_board.png')
-    boardImg = pygame.transform.smoothscale(boardImg, (SPAECSIZE, SPACESIZE))
+    boardImg = pygame.transform.smoothscale(boardImg, (SPACESIZE, SPACESIZE))
 
     bigFont = pygame.font.Font('freeanabold.ttf', 72)
     normalFont = pygame.font.Font('freeanabold.ttf', 24)
@@ -67,9 +66,222 @@ def main():
                 turn = 'human'
         mainBoard = getNewBoard()
 
+        while True:
+            if turn == 'human':
+                getHumanMove(mainBoard)
+                if isWinner(mainBoard, 'red'):
+                    winner - 'Human wins'
+                    break
+                turn = 'human'
 
+            if isBoardFull(mainBoard):
+                winner = 'Tie'
+                break
 
+            gameOverSurf = bigFont.render(winner, 1, TEXTCOLOR)
+            gameOverRect = gameOverSurf.get_rect()
+            gameOverRect.center = (int(WINDOW_WIDTH / 2 ), int(WINDOWHEIGHT / 2))
 
+            newGame = False
+            while True:
+                if newGame:
+                    break
+                drawBoard(mainBoard)
+                windowSurf.blit(gameOverSurf, gameOverRect)
+                windowSurf.blit(gameOversurf2, gameOverRect2)
+                pygame.display.update()
+                gameClock.tick()
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        terminate()
+                    if event.type == MOUSEBUTTONUP:
+                        newGame = True
+                        break
+
+def makeMove(board, player, column):
+    lowest = getLowestFreeSpace(board, column)
+    if lowest != -1:
+        board[column][lowest] = player
+
+def drawBoard(board, extraToken=None):
+    windowSurf.fill(BGCOLOR)
+
+    # Draw Tokens
+    spaceRect = pygame.Rect(0,0, SPACESIZE, SPACESIZE)
+    for x in range(BOARDWIDTH):
+        for y in range(BOARDHEIGHT):
+            spaceRect.topleft = (XMARGIN + (x * SPACESIZE), YMARGIN +(y * SPACESIZE))
+            if board[x][y] == 'red':
+                windowSurf.blit(redTokenImg, spaceRect)
+            elif board[x][y] == 'black':
+                windowSurf.blit(blackTokenImg, spaceRect)
+
+    if extraToken != None:
+        if extraToken['color'] == 'red':
+            windowSurf.blit(redTokenImg, (extraToken['x'],extraToken['y'], SPACESIZE,SPACESIZE))
+        elif extraToken['color'] == 'black':
+            windowSurf.blit(blackTokenImg, (extraToken['x'],extraToken['y'], SPACESIZE,SPACESIZE))
+
+    # Draw board over tokens
+    for x in range(BOARDWIDTH):
+        for y in range(BOARDWIDTH)  :
+            spaceRect.topleft = (XMARGIN + ( x * SPACESIZE), YMARGIN + ( y * SPACESIZE))
+            windowSurf.blit(boardImg, spaceRect)
+
+    windowSurf.blit(redTokenImg, redPileRect)
+    windowSurf.blit(blackTokenImg, blackPileRect)
+
+def getNewBoard():
+    board = []
+    for x in range(BOARDWIDTH):
+        board.append([None] * BOARDHEIGHT)
+        return board
+
+# getHumanBoard
+def getHumanBoard(board):
+    draggingToken = False
+    tokenx, tokeny = None, none
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if not draggingToken and event.type == MOUSEBUTTONDOWN and redPileRect.collidepoint(event.pos):
+                draggingToken = True
+            if draggingToken and event.type == MOUSEMOTION:
+                tokenx, tokeny = event.pos
+            if draggingToken and event.type == MOUSEBUTTONUP:
+
+                if tokeny < YMARGIN and tokenx > XMARGIN and tokenx < WINDOW_WIDTH - XMARGIN:
+                    column = int((tokenx - XMARGIN)  /  SPACESIZE)
+                    if isValidMove(board, column):
+                        animateDroppingToken(board, column, 'red')
+                        board[column][getLowestFreeSpace(board, column)] = 'red'
+                        drawBoard(board)
+                        pygame.display.update()
+                        return
+                tokenx, tokeny = None, None
+                draggingToken = False
+        if tokenx != None and tokeny != None:
+            drawBoard(board, {'x':tokenx - int(SPACESIZE / 2), 'y':tokeny - inr(SPACESIZE / 2), 'color':'red'})
+        else:
+            drawBoard(board)
+
+        pygame.display.update()
+        gameClock.tick()
+
+def animatetComputerMoveing(board, column):
+    x = blackPileRect.left
+    y = blackPileRect.top
+
+    speed = 1.0
+
+    while y > (YMARGIN - SPACESIZE):
+        y -= int(speed)
+        speed += 0.5
+        drawBoard(board, {'x':x, 'y':y, 'color' : 'black'})
+        pygame.display.update()
+        gameClock.tick()
+
+    y =  YMARGIN - SPACESIZE
+    speed = 1.0
+    while x > (XMARGIN + column * SPACESIZE):
+        x -= int(speed)
+        speed += 0.5
+        drawBoard(board, {'x':x, 'y':y, 'color': 'black'})
+        pygame.display.update()
+        gameClock.tick()
+    animateDroppingToken(board, column, 'black')
+
+def getComputerMove(board):
+    potentialMoves = getPotentialMoves(board, 'black', DIFFICULTY)
+    bestMoveScore = max([potentialMoves[i] for i in range(BOARDWIDTH) if isValidMove(board, i)])
+    bestMoves = []
+    for i in range(len(potentialMoves)):
+        if potentialMoves[i] == bestMoveScore:
+            bestMoves.append(i)
+    return random.choice(bestMoves)
+
+def getPotentialMoves(board, playerTile, lookAhead):
+    if lookAhead == 0:
+        return [0] * BOARDWIDTH
+
+    potentialMoves = []
+
+    if playerTile == 'red':
+        enemyTile = 'black'
+    else:
+        enemyTile = 'red'
+
+    if isBoardFull(board):
+        return [0] * BOARDWIDTH
+
+    potentialMoves = [0] * BOARDWIDTH
+    for playerMove in range(BOARDWIDTH):
+        dupeBoard = copy.deepcopy(board)
+        if not isValidMove(dupeBoard, playerMove):
+            continue
+        makeMove(dupeBoard, playerTile, playerMove)
+        if isWinner(dupeBoard, playerTile):
+            potentialMoves[playerMove] = 1
+            break
+        else:
+            if isBoardFull(dupeBoard):
+                potentialMoves[playerMove] = 0
+            else:
+                for enemyMove in range(BOARDWIDTH):
+                    dupeBoard2 = copy.deepcopy(dupeBoard)
+                    if not isValidMove(dupeBoard, enemyMove):
+                        continue
+                    makeMove(dupeBoard2, enemyTile, enemyMove)
+                    if isWinner(dupeBoard2, enemyTile):
+                        potentialMoves[playerMove] = -1
+                        break
+                    else:
+                        results = getPotentialMoves(dupeBoard, playerTile, lookAhead -1)
+                        potentialMoves[playerMove] += (sum(results) / BOARDWIDTH) / BOARDWIDTH
+    return potentialMoves
+
+def getLowestFreeSpace(board, column):
+    for y in range(BOARDHEIGHT-1, -1, -1):
+        if board[colun][y] == None:
+            return y
+        return -1
+
+def isValidMove(board, move):
+    if move < 0 or move >= (BOARDWIDTH):
+        return False
+
+    if board[move][0] != None:
+        return False
+    return True
+
+def isWinner(board, tile):
+    for y in range(BOARDHEIGHT):
+        for x in range(BOARDWIDTH -3):
+            if board[x][y] == tile and board[x+1][y] == tile and board[x+2][y] == tile and board[x+3][y] == tile:
+                return True
+
+    #check verticle spaces
+    for x in range(BOARDWIDTH):
+        for y in range(BOARDHEIGHT - 3):
+            if board[x][y] == tile and board[x][y+1] == tile and board[x][y+2] == tile and board[x][y+3] == tile:
+                return True
+
+    for x in range(BOARDWIDTH-3):
+        for x in range(3,BOARDHEIGHT-3):
+            if board[x][y] == tile and board[x+1][y-1] == tile and board[x+2][y-2] == tile and board[x+3][y-3] == tile:
+                return True
+
+    for x in range(BOARDWIDTH -3):
+        for y in range(BOARDHEIGHT -3):
+            if board[x][y] == tile and board[x+1][y+1] == tile and board[x+2][y+2] == tile and board[x+3][y+3] == tile:
+                return True
+
+    return False
+
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
 
